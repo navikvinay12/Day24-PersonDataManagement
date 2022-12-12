b70ca913bb0fda57028c9575051f899f4708c2b2
Vinay <navik.vinay12@gmail.com>
2022-12-12 15:20:00 +0530

    creating a person than list of person

---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaDemo222Batch
+{
+    public class Person
+    {
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return $"SerialNo:{SSN} Name:{Name} Age:{Age} Address:{Address}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LambdaDemo222Batch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>();
+            AddingPersonDetailsIntoList(list);
+            Console.WriteLine();
+        }
+        public static void AddingPersonDetailsIntoList(List<Person> list)
+        {
+            list.Add(new Person() { SSN = 1, Name = "Kavya", Address = "bnglr", Age = 12, });
+            list.Add(new Person() { SSN = 2, Name = "UV", Address = "Karnataka", Age = 70, });
+            list.Add(new Person() { SSN = 3, Name = "Arun", Address = "Mumbai", Age = 32, });
+            list.Add(new Person() { SSN = 4, Name = "Kalpan", Address = "Bangluru", Age = 16, });
+            list.Add(new Person() { SSN = 5, Name = "Sachin", Address = "Bangluru", Age = 63, });
+            list.Add(new Person() { SSN = 6, Name = "Arun", Address = "Chennai", Age = 36, });
+            list.Add(new Person() { SSN = 7, Name = "Shrikanth", Address = "India", Age = 14, });
+            DisplayPersonDetails(list);
+        }
+        public static void DisplayPersonDetails(List<Person> list)
+        {
+            foreach(Person person in list)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
